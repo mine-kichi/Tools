d52e5df2e00a00effaca0c2c75bb5b45813ebe80
mine-kichi <erhabenwille@hotmail.com>
2023-01-05 19:12:18 +0900

    add summary_extractor

---
+++ summary_extractor/summary_extractor/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace summary_extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.make_file = new System.Windows.Forms.Button();
+            this.extact = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // make_file
+            // 
+            this.make_file.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.make_file.Location = new System.Drawing.Point(24, 39);
+            this.make_file.Name = "make_file";
+            this.make_file.Size = new System.Drawing.Size(327, 32);
+            this.make_file.TabIndex = 0;
+            this.make_file.Text = "make file";
+            this.make_file.UseVisualStyleBackColor = true;
+            this.make_file.Click += new System.EventHandler(this.makeFileBtn);
+            // 
+            // extact
+            // 
+            this.extact.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.extact.Location = new System.Drawing.Point(22, 95);
+            this.extact.Name = "extact";
+            this.extact.Size = new System.Drawing.Size(329, 30);
+            this.extact.TabIndex = 1;
+            this.extact.Text = "extract";
+            this.extact.UseVisualStyleBackColor = true;
+            this.extact.Click += new System.EventHandler(this.extractBtn);
+            // 
+            // close
+            // 
+            this.close.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.close.Location = new System.Drawing.Point(24, 203);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(329, 30);
+            this.close.TabIndex = 2;
+            this.close.Text = "close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.closeBtn);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(24, 147);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(244, 32);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(392, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(270, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(19, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(290, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "display";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "md";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "(*.md)|*.md";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 245);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.extact);
+            this.Controls.Add(this.make_file);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button make_file;
+        private System.Windows.Forms.Button extact;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ summary_extractor/summary_extractor/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace summary_extractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 日々書き込んでいくファイル作成関数。
+        /// フォルダ指定機能作成予定
+        /// ファイル名も日付にする予定
+        /// </summary>
+        private void makeFileBtn(object sender, EventArgs e)
+        {
+
+            // 日付取得
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt.ToString("yyyy/MM/dd"));
+            string path = dt.ToString("yyyyMMdd") + ".md";
+            // FileInfoのインスタンスを生成する
+            FileInfo fileInfo = new FileInfo(path);
+            if (File.Exists(path))
+            {
+                MessageBox.Show("日付ファイルはすでに存在します");
+            }
+            else {
+                // ファイルを作成する
+                FileStream fileStream = fileInfo.Create();
+            }
+
+            // ToDo
+            path = "ToDo.md";
+            // FileInfoのインスタンスを生成する
+            fileInfo = new FileInfo(path);
+            if (File.Exists(path))
+            {
+                MessageBox.Show("ToDoファイルはすでに存在します");
+            }
+            else
+            {
+                // ファイルを作成する
+                FileStream fileStream = fileInfo.Create();
+            }
+
+            // QA
+            path = "QA.md";
+            // FileInfoのインスタンスを生成する
+            fileInfo = new FileInfo(path);
+            if (File.Exists(path))
+            {
+                MessageBox.Show("QAファイルはすでに存在します");
+            }
+            else
+            {
+                // ファイルを作成する
+                FileStream fileStream = fileInfo.Create();
+            }
+
+            // other
+            path = "other.md";
+            // FileInfoのインスタンスを生成する
+            fileInfo = new FileInfo(path);
+            if (File.Exists(path))
+            {
+                MessageBox.Show("otherファイルはすでに存在します");
+            }
+            else
+            {
+                // ファイルを作成する
+                FileStream fileStream = fileInfo.Create();
+            }
+
+
+        }
+
+        /// <summary>
+        /// 抽出ボタン実行部
+        /// </summary>
+        private void extractBtn(object sender, EventArgs e)
+        {
+            int flg = 0;
+            int type = 0;
+            List<string> list = new List<string>();
+            // ファイルを一行ずつ読み込み
+            foreach (string line in System.IO.File.ReadLines(@"./20230104.md"))
+            {
+                if( flg == 0)
+                {
+                    //<Todo/>開始
+                    //</Todo>終わり
+                    if (line == "<Todo/>")
+                    {
+                        type = 1;
+                        flg = 1;
+                    }else if(line == "<QA/>"){
+                        type = 2;
+                        flg = 1;
+                    }else if(line == "<other/>"){
+                        type = 3;
+                        flg = 1;
+                    }
+
+                }
+                else
+                {
+                    if (line == "</Todo>" || line == "</QA>" || line == "</other>")
+                    {
+                        additionSentence(flg, list);
+                        //初期化
+                        type = 0;
+                        flg = 0;
+                        list = new List<string>(); 
+                    }
+
+                    list.Add(line);
+
+                }
+
+            }
+
+        }
+
+        /// <summary>
+        /// ファイルに抽出事項を追記する関数
+        /// </summary>
+        private void additionSentence(int flg, List<string> S)
+        {
+            string path;
+            // ファイルオープン
+            if(flg == 1)
+            {
+                path = @"ToDo.md";
+            }
+            else if( flg == 2)
+            {
+                path = @"QA.md";
+
+            }
+            else
+            {
+                path = @"other.md";
+            }
+
+            // 追記
+            foreach (string s in S)
+            {
+
+                File.AppendAllText(path, s+Environment.NewLine);
+            }
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void closeBtn(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// close
+        /// </summary>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string openFileName;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileName = openFileDialog1.FileName;
+            }
+            else
+            {
+                return;
+            }
+
+            textBox1.Clear();
+            textBox1.Text = openFileName;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+            pInfo.FileName = "notepad";
+            pInfo.Arguments = textBox1.Text;
+
+            Process.Start(pInfo);
+        }
+    }
+}
